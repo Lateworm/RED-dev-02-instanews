735fa434bffd09d1f35a5aa23fef80af539ddcf9
Late Worm <travis.rempel@gmail.com>
2017-10-19 16:25:54 -0700

    Comment out console.logs and add some ideas in comments

--- js/scripts.js
+++ js/scripts.js
@@ -33,7 +33,7 @@ var sections = [
 
 // Build the list of selectable sections
 var numSections = sections.length;
-var n;
+var n = 5;
 for (n=0; n<numSections; n++) {
 	var option = sections[n];
 	$('select').append('<option value="' + option + '">' + option + '</option>');
@@ -45,8 +45,8 @@ $('#selection').on('change', function() {
 	// console.log('Received a section selection: ' + selection);
 	var apiKey= '3d0a4529188c480899c9ae22d7122aae'; // API Key for Top Stories:
 	var apiUrl = 'https://api.nytimes.com/svc/topstories/v2/' + selection + '.json?api-key=' + apiKey; // Build a keyed API URL for the selected section
-	console.log('Generated keyed URL:' + apiUrl);
-	console.log('Fetching JSON object...');
+	// console.log('Generated keyed URL:' + apiUrl);
+	// console.log('Fetching JSON object...');
 
 
 	$.ajax({
@@ -54,17 +54,42 @@ $('#selection').on('change', function() {
 		method: 'GET',
 
 	}).done(function(data) {
-		console.log(data);
+		// console.log(rawData);
+		$('article').empty();
+
+		// try filtering the data before we begin looping
+		// hints: .fiter() and .slice()
+		// var articleMediaLength = rawData.results[n].multimedia.length;
+		// console.log(articleMediaLength);
+		// var data = rawData.results;
+
+		// $.each(data, function(index, value) {
+		// 	var articleTitle = data.title;
+		// 	console.log(articleTitle);
+		// });
+
+		
+
 
 		for (n=0; n<12; n++) {
+
 			var o = n+1;
 			var articleTitle = data.results[n].title;
 			var articleAbstract = data.results[n].abstract;
 			var articleByline = data.results[n].byline;
 			var articleUrl = data.results[n].url;
+			//var articleMediaLength = data.results[n].multimedia.length;
 			var articleImage = data.results[n].multimedia[4].url;
+
+			$('header').removeClass('header-initial').addClass('header-loaded');
 			$('article').append('<a href="' + articleUrl + '" style="background-image: url(' + articleImage + ');"><div class="overlay"><h2>' + o + ': ' + articleTitle + '</h2><p>' + articleAbstract + '</p><p class="byline">' + articleByline + '</p></div></a>');
+
 		}
+
+
+
+
+
 
 	}).fail(function() {
 		$('header').append('<p class="error">Error</p>')
