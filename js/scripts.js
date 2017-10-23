ab8363f0b44c4611279a57ddc96906cab9305d3a
Late Worm <travis.rempel@gmail.com>
2017-10-23 11:54:37 -0700

    Implement document.ready and call overlay animation inside of .always()

--- js/scripts.js
+++ js/scripts.js
@@ -1,107 +1,115 @@
 //jQuery document ready function
-// $(function() { stuff and junk });
+$(function() {
 
-var sections = [
-'home',
-'arts',
-'automobiles',
-'books',
-'business',
-'fashion',
-'food',
-'health',
-'insider',
-'magazine',
-'movies',
-'national',
-'ny region',
-'obituaries',
-'opinion',
-'politics',
-'real estate',
-'science',
-'sports',
-'sunday review',
-'t magazine',
-'technology',
-'theater',
-'travel',
-'upshot',
-'world',
-];
+	$('.loading').hide();
+	$('.error').hide();
 
-// Build the list of selectable sections
-var numSections = sections.length;
-var n = 5;
-for (n=0; n<numSections; n++) {
-	var option = sections[n];
-	$('select').append('<option value="' + option + '">' + option + '</option>');
-}
+	var sections = [
+	'home',
+	'arts',
+	'automobiles',
+	'books',
+	'business',
+	'fashion',
+	'food',
+	'health',
+	'insider',
+	'magazine',
+	'movies',
+	'national',
+	'ny region',
+	'obituaries',
+	'opinion',
+	'politics',
+	'real estate',
+	'science',
+	'sports',
+	'sunday review',
+	't magazine',
+	'technology',
+	'theater',
+	'travel',
+	'upshot',
+	'world',
+	];
 
+	// Build the list of selectable sections
+	var numSections = sections.length;
+	var n = 5;
+	for (n=0; n<numSections; n++) {
+		var option = sections[n];
+		$('select').append('<option value="' + option + '">' + option + '</option>');
+	}
 
+	$('#selection').on('change', function() {
+		// $('article').empty();
+		$('.loading').show();
+		$('.error').hide();
+		var selection = $('#selection').val(); // Get the user's section selection
+		selection = selection.replace(/\s+/g, ''); // Remove spaces so it'll work in the URL
+		var apiKey= '3d0a4529188c480899c9ae22d7122aae'; // API Key for Top Stories:
+		var apiUrl = 'https://api.nytimes.com/svc/topstories/v2/' + selection + '.json?api-key=' + apiKey; // Build a keyed API URL for the selected section
 
-$('#selection').on('change', function() {
-	// $('article').empty();
-	// Construct the loading spinner
-	var load = '<div class="loading"><img src="img/loading_trans_75px.gif" alt="Loading" /></div>';
-	$('article').append(load);
-	var selection = $('#selection').val(); // Get the user's section selection
-	selection = selection.replace(/\s+/g, ''); // Remove spaces so it'll work in the URL
-	var apiKey= '3d0a4529188c480899c9ae22d7122aae'; // API Key for Top Stories:
-	var apiUrl = 'https://api.nytimes.com/svc/topstories/v2/' + selection + '.json?api-key=' + apiKey; // Build a keyed API URL for the selected section
+		// Pull the API info
 
-	// Pull the API info
+		$.ajax({
+			url: apiUrl,
+			method: 'GET',
 
-	$.ajax({
-		url: apiUrl,
-		method: 'GET',
+		}).done(function(data) {
+			// console.log(data);
+			$('article').empty();
 
-	}).done(function(data) {
-		// console.log(data);
-		$('article').empty();
+			var anchorsAppended = 0;
+			$.each(data.results, function(each) {
 
-		var anchorsAppended = 0;
-		$.each(data.results, function(each) {
+				var articleMediaLength = data.results[each].multimedia.length;
+				var articleMediaIndex = articleMediaLength - 1;
+				if (articleMediaLength === 0) {
+					return true;
+				}
+				anchorsAppended++;
+				if (anchorsAppended <= 12) {
+					// console.log('Appending anchor '+anchorsAppended+' from .results['+each+']...');
+					var articleTitle = data.results[each].title;
+					var articleAbstract = data.results[each].abstract;
+					// var articleByline = data.results[each].byline;
+					var articleUrl = data.results[each].url;
+					var articleImage = data.results[each].multimedia[articleMediaIndex].url; // use the best available media
+					$('header').removeClass('header-initial').addClass('header-loaded');
+					var post = '<a href="' + articleUrl + '" style="background-image: url(' + articleImage + ');">';
+					post += '<div class="overlay">';
+					post += '<h2>' + articleTitle + '</h2>';
+					post += '<p>' + articleAbstract + '</p>';
+					// post += '<p class="byline">' + articleByline + '</p>';
+					post += '</div></a>';
+					$('article').append(post);
+					// $('article').append('<p>This is a paragraph added via jQuery</p>');
+				}
 
-			var articleMediaLength = data.results[each].multimedia.length;
-			if (articleMediaLength === 0) {
-				return true;
-			}
-			anchorsAppended++;
-			if (anchorsAppended <= 12) {
-				// console.log('Appending anchor '+anchorsAppended+' from .results['+each+']...');
-				var articleTitle = data.results[each].title;
-				var articleAbstract = data.results[each].abstract;
-				// var articleByline = data.results[each].byline;
-				var articleUrl = data.results[each].url;
-				var articleImage = data.results[each].multimedia[4].url; // [4] = hi-rez, [3] = lo-rez
-				$('header').removeClass('header-initial').addClass('header-loaded');
-				var post = '<a href="' + articleUrl + '" style="background-image: url(' + articleImage + ');">';
-				post += '<div class="overlay">';
-				post += '<h2>' + articleTitle + '</h2>';
-				post += '<p>' + articleAbstract + '</p>';
-				// post += '<p class="byline">' + articleByline + '</p>';
-				post += '</div></a>';
-				// $('article').append(post);
-				$('article').append('<p>This is a paragraph added via jQuery</p>');
-			}
-			return (anchorsAppended !== 12);
-		})
-	}).fail(function() {
-		$('label').text('Error').addClass('error');
-		setTimeout(function () {
-			$('label').removeClass('error').text('Choose a section'); }, 3000);
+				return (anchorsAppended <= 12);
+			})
+		}).fail(function() {
+			$('.error').show();
+		}).always(function() {
+			$('.loading').hide();
+			addListener();
+		});
+		
 	});
-});
 
-// Show the overlay on mouseover
+	// Show the overlay on mouseover
 
-$('article').children().on('mouseover', function(e) {
-	e.preventDefault();
-	$(this).children().toggleClass('toggle-height');
-	console.log('mouseover');
-});
-$('article').children().on('mouseout', function(e) {
-	e.preventDefault();
-	$(this).children().toggleClass('toggle-height');
-});
+	function addListener(){
+	$('article').children().on('mouseover', function(e) {
+		e.preventDefault();
+		$(this).children().toggleClass('toggle-height');
+	});
+	$('article').children().on('mouseout', function(e) {
+		e.preventDefault();
+		$(this).children().toggleClass('toggle-height');
+	});
+
+	}
+
+}); // End of document.ready
