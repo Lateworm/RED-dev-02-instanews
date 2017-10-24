0fa73cd48a22c4ab02f338c4b10af9e46439a640
Late Worm <travis.rempel@gmail.com>
2017-10-23 21:20:58 -0700

    Begin setup for improved section listing

--- js/scripts.js
+++ js/scripts.js
@@ -5,41 +5,78 @@ $(function() {
 	$('.error').hide();
 
 	var sections = [
-	// 'home',
-	'arts',
-	'automobiles',
-	'books',
-	// 'business',
-	'fashion',
-	'food',
-	// 'health',
-	// 'insider',
-	// 'magazine',
-	'movies',
-	// 'national',
-	// 'ny region',
-	// 'obituaries',
-	// 'opinion',
-	// 'politics',
-	// 'real estate',
-	'science',
-	// 'sports',
-	// 'sunday review',
-	// 't magazine',
-	'technology',
-	'theater',
-	'travel',
-	// 'upshot',
-	'world',
+		// 'home',
+		'arts',
+		'automobiles',
+		'books',
+		// 'business',
+		'fashion',
+		'food',
+		// 'health',
+		// 'insider',
+		// 'magazine',
+		'movies',
+		// 'national',
+		// 'ny region',
+		// 'obituaries',
+		// 'opinion',
+		// 'politics',
+		// 'real estate',
+		'science',
+		// 'sports',
+		// 'sunday review',
+		// 't magazine',
+		'technology',
+		'theater',
+		'travel',
+		// 'upshot',
+		'world',
 	];
 
 	// Build the list of selectable sections
 	var numSections = sections.length;
-	var n = 5;
+	// var n = 5;
 	for (n=0; n<numSections; n++) {
 		var option = sections[n];
 		$('select').append('<option value="' + option + '">' + option + '</option>');
 	}
+
+	// var sectionsExperimental = [
+	// 	{'section': 'home', 'visible': false,},
+	// 	{'section': 'arts', 'visible': false,},
+	// 	{'section': 'automobiles', 'visible': true,},
+	// 	{'section': 'books', 'visible': false,},
+	// 	{'section': 'business', 'visible': false,},
+	// 	{'section': 'fashion', 'visible': false,},
+	// 	{'section': 'food', 'visible': true,},
+	// 	{'section': 'health', 'visible': false,},
+	// 	{'section': 'insider', 'visible': false,},
+	// 	{'section': 'magazine', 'visible': false,},
+	// 	{'section': 'movies', 'visible': false,},
+	// 	{'section': 'national', 'visible': false,},
+	// 	{'section': 'ny region', 'visible': false,},
+	// 	{'section': 'obituaries', 'visible': false,},
+	// 	{'section': 'opinion', 'visible': false,},
+	// 	{'section': 'politics', 'visible': false,},
+	// 	{'section': 'real estate', 'visible': true,},
+	// 	{'section': 'science', 'visible': false,},
+	// 	{'section': 'sports', 'visible': false,},
+	// 	{'section': 'sunday review', 'visible': false,},
+	// 	{'section': 't magazine', 'visible': false,},
+	// 	{'section': 'technology', 'visible': true,},
+	// 	{'section': 'theater', 'visible': false,},
+	// 	{'section': 'travel', 'visible': false,},
+	// 	{'section': 'upshot', 'visible': false,},
+	// 	{'section': 'world', 'visible': true,},
+	// ];
+
+	// // Build the experimental list of selectable sections
+	// var numSections = experimentalSections.length;
+	// $.each(sectionsExperimental, function(each) {
+	// 	var section = sectionsExperimental[each].section;
+	// 	var visible = sectionsExperimental[each].visible;
+	// 	$('select').append('<option value="' + option + '">' + option + '</option>');
+	// }
 
 	$('#selection').on('change', function() {
 		// $('article').empty();
@@ -49,6 +86,7 @@ $(function() {
 		selection = selection.replace(/\s+/g, ''); // Remove spaces so it'll work in the URL
 		var apiKey= '3d0a4529188c480899c9ae22d7122aae'; // API Key for Top Stories:
 		var apiUrl = 'https://api.nytimes.com/svc/topstories/v2/' + selection + '.json?api-key=' + apiKey; // Build a keyed API URL for the selected section
+		console.log(apiUrl);
 
 		// Pull the API info
 
